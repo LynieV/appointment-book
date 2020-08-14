645a0e83415effd04e5604ee0048ffa7b92204cf
LynieV <vmcclimon33@yahoo.com>
2020-08-13 19:09:37 -0500

    added username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :appointments
     has_many :clients, through: :appointments
 
+    validates :username, uniqueness: true, presence: true
+
     has_secure_password
 
 end
