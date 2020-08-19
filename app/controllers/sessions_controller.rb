e38e4c654635d092a0f564b6d2159998d73a9ed9
LynieV <vmcclimon33@yahoo.com>
2020-08-18 20:44:21 -0400

    coded create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,17 @@ class SessionsController < ApplicationController
     end
 
     def create
+        #try to find the user in the system
+        @user = User.find_by(username: params[:user][:username])
+
+        if @user.try(:authenticate, params[:user][:password]
+            session[:user_id] = @user.id
+            redirect_to user_path
+        else
+            flash[:error] = "Please use correct username and password"
+            redirect_to login_path
+        end
     end
+
 
 end
