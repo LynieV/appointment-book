c07ea2dde21c9692ba1491808e0e47a011119487
LynieV <vmcclimon33@yahoo.com>
2020-09-02 20:43:14 -0400

    added cresteand params methods

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,6 +2,24 @@ class ClientsController < ApplicationController
 
     def new
         @client = Client.new
+        @client.build_appointment
+    end
+
+    def create
+        @client = Client.new(client_params)
+        if @client.save
+            redirect_to user_path(@client)
+        else
+            render :new
+        end
+    end
+
+
+    private
+
+    def client_params
+        params.require(:client).permit(:first_name, :last_name, :appointment_id
+        appointmnet_attributes: [:time])
     end
 
 end
